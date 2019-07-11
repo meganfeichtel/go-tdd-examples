1c57078078559c6102463fda289cdde7459e314a
meganfeichtel <mfeichtel@redventures.com>
2019-07-11 15:23:21 -0400

    refactored function to use RomanNumeral struct abstraction instead of switch case statement

--- numerals.go
+++ numerals.go
@@ -2,27 +2,29 @@ package numerals
 
 import "strings"
 
+/*RomanNumeral defines a struct to represent a roman numeral and its integer value*/
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+/*RomanNumerals cointains an array of roman numeral structs*/
+var RomanNumerals = []RomanNumeral{
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 /*ConvertToRoman converts a given int to roman numeral*/
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
-	for arabic > 0 {
-		switch {
-		case arabic > 9:
-			result.WriteString("X")
-			arabic -= 10
-		case arabic > 8:
-			result.WriteString("IX")
-			arabic -= 9
-		case arabic > 4:
-			result.WriteString("V")
-			arabic -= 5
-		case arabic > 3:
-			result.WriteString("IV")
-			arabic -= 4
-		default:
-			result.WriteString("I")
-			arabic--
+	for _, numeral := range RomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
 		}
 	}
 
